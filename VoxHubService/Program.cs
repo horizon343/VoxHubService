9505357c193212c9decf48eeb7b91205105dbbc1
horizon343 <sh.maksim343@gmail.com>
2026-03-24 16:54:55 +0500

    clear

--- VoxHubService/Program.cs
+++ VoxHubService/Program.cs
@@ -1,5 +1,3 @@
-using VoxHubService.Services;
-
 namespace VoxHubService;
 
 public class Program
@@ -8,16 +6,9 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddGrpc();
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        app.MapGrpcService<GreeterService>();
-        app.MapGet("/",
-            () =>
-                "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
         app.Run();
     }
