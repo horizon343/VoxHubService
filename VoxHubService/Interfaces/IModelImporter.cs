fdf7c636a71caa46fbdbbaa458d03f50dfc3eadc
horizon343 <sh.maksim343@gmail.com>
2026-04-21 18:16:23 +0500

    feat: IModelImporter

---
+++ VoxHubService/Interfaces/IModelImporter.cs
@@ -0,0 +1,11 @@
+﻿using VoxHubService.Domain;
+
+namespace VoxHubService.Interfaces;
+
+public interface IModelImporter
+{
+    Task<VoxelModel> ImportAsync(
+        Stream input,
+        CancellationToken cancellationToken = default
+    );
+}
