947de2d6c052f4004bf7709c3700f1a1b33d0b5e
horizon343 <sh.maksim343@gmail.com>
2026-04-22 19:36:39 +0500

    feat: Manifest

---
+++ VoxHubService/Domain/Versioning/ManifestChunk.cs
@@ -0,0 +1,9 @@
+﻿using VoxHubService.Domain.Chunking;
+
+namespace VoxHubService.Domain.Versioning;
+
+public sealed record ManifestChunk(
+    ChunkKey Key,
+    string Hash,
+    string ObjectKey
+);
---
+++ VoxHubService/Domain/Versioning/VersionManifest.cs
@@ -0,0 +1,10 @@
+﻿namespace VoxHubService.Domain.Versioning;
+
+public sealed class VersionManifest
+{
+    public required Guid VersionId { get; init; }
+    public Guid? BaseSnapshotVersionId { get; init; }
+    public string? SnapshotObjectKey { get; init; }
+    public string? DeltaObjectKey { get; init; }
+    public required IReadOnlyList<ManifestChunk> Chunks { get; init; }
+}
