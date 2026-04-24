1e781944594251570b62a548be1b2342c881da2c
horizon343 <sh.maksim343@gmail.com>
2026-04-24 17:40:14 +0500

    feat: IObjectStorage

---
+++ VoxHubService/Interfaces/IObjectStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace VoxHubService.Interfaces;
+
+public interface IObjectStorage
+{
+    Task PutAsync(string key, Stream content, CancellationToken cancellationToken = default);
+    Task<Stream> GetAsync(string key, CancellationToken cancellationToken = default);
+    Task<bool> ExistsAsync(string key, CancellationToken cancellationToken = default);
+    Task DeleteAsync(string key, CancellationToken cancellationToken = default);
+}
